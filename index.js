5265e7e6a2bb7f4ef109370df25ccbef9b765086
Innocentaman <amanvyas530@gmail.com>
2025-01-02 17:25:22 +0530

    ff

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
+
 dotenv.config({});
 
- 
 const PORT = process.env.PORT || 5000;
 
 // middleware
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
+
+// CORS Configuration
+const corsOption = {
+    origin: ['http://localhost:3000', 'https://67767c5097d6eaf9fbfb0be2--chipper-salamander-7245bf.netlify.app'], // Add allowed domains here
+    credentials: true
 };
 app.use(cors(corsOption)); 
 
-
 // routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
+app.use("/api/v1/user", userRoute); 
+app.use("/api/v1/message", messageRoute);
 
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
     connectDB();
-    console.log(`Server listen at prot ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
