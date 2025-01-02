cfa58e089e516ee3550b583f57a880b628cd5a75
Innocentaman <amanvyas530@gmail.com>
2025-01-02 22:31:33 +0530

    ff

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['http://localhost:3000','https://67767c5097d6eaf9fbfb0be2--chipper-salamander-7245bf.netlify.app'],
         methods:['GET', 'POST'],
     },
 });
